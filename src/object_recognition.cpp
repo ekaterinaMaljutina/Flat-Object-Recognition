5b49b0053ffdf65afec359766dd35b31e4555f65
unknown <ss2015@ws-2k-110-n04.ccam.cmc.unn.net>
2015-08-24 14:34:24 +0300

    add sufr

--- src/object_recognition.cpp
+++ src/object_recognition.cpp
@@ -1,3 +1,27 @@
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+
+#include "vector"
+
+using namespace cv;
+
+void FindFeatureUseSURF(const Mat image)
+{
+	int minHessian = 400;
+
+	SurfFeatureDetector detector( minHessian);
+
+	vector<KeyPoint> keypoint_object;
+
+	detector.detect(image,keypoint_object);
+
+
+}
+
+
 int main(int argc, const char **argv)
 {   
     return 0;
